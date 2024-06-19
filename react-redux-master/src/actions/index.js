03cc774026f56129a259f7df466d0b11fd8bb550
VeronicaVB <verisb@gmail.com>
2024-06-19 10:06:17 +1000

    Counter using redux

--- react-redux-master/src/actions/index.js
+++ react-redux-master/src/actions/index.js
@@ -1,1 +1,8 @@
-//actions
+const increment = (val) => {
+    return {
+        type: 'INCREMENT',
+        inc: val // val is the value you want to increase the counter by everytime the button is clicked. 
+    }
+}
+
+export default increment;
